31c4e5c1993dd0416875336f3426d31f5ec54416
Ian Goodwin <IG055169@psd.provo.edu>
2018-02-14 08:46:54 -0700

    Finished String sorter, working on file thing

--- RadixStringTester.java
+++ RadixStringTester.java
@@ -10,33 +10,37 @@ public class RadixStringTester {
     }
     public static void sort(String ary[]) { //ascending order Radix Sort
         ArrayList<String>[] chars = (ArrayList<String>[])new ArrayList[26];
-        int mask = 1; //Initially looks at the LSB.
-        for(int i = 0; i < 31; i ++) {
-            //Cycle through all 31 bits of the integers (but not the sign bit) starting with
-            //the LSB.
-            zeroes.clear();
-            ones.clear();
+        for(int i = 0; i < chars.length; i++) {
+            chars[i] = new ArrayList<String>();
+        }
+        int mask = 3; //Initially looks at the LSB.
+        for(int i = 0; i < 4; i ++) {
+            //Cycle through all 4 letters of the Strings starting with
+            //the last.
+            for(int k = 0; k < chars.length; k++) {
+                System.out.println(k);
+                chars[k].clear();
+            }
             int zerosArrayCount = 0, onesArrayCount = 0;
             for(int j = 0; j < ary.length; j++) {
                 //Cycle through each item in the array to be sorted
-                int testBit = ary[j] & mask; //Apply mask to test a particular bit.
-                if(testBit == 0) {
-                    zeroes.add(ary[j]);
-                    zerosArrayCount++;
-                } else {
-                    ones.add(ary[j]);
-                    onesArrayCount++;
-                }
+                char test = ary[j].charAt(mask); //Apply mask to test a particular character.
+                chars[test-65].add(ary[j]);
             }
 
             //Copy latest progress back to the array. For descending sort, switch the
             //two loops below.
             int indx = 0;
-            for(int j = 0; j < zerosArrayCount; j++)
+            for(int j = chars.length - 1; j >= 0; j--) {
+                for(int k = 0; k < chars[j].size(); k++) {
+                    ary[indx++] = chars[j].get(k);
+                }
+            }
+            /*for(int j = 0; j < zerosArrayCount; j++)
             ary[indx++] = zeroes.get(j);
             for(int j = 0; j < onesArrayCount; j++)
-            ary[indx++] = ones.get(j);
-            mask *= 2; //Advance the mask to the next bit
+            ary[indx++] = ones.get(j);*/
+            mask--; //Advance the mask to the next bit
         }
     }
 }
